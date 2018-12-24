50f521d948f11f30771de35e8aab8dd5e5f8be05
Anna Yasenova <yasenova2013@gmail.com>
2018-12-24 22:14:30 +0200

    Structure done.

---
+++ mini-games/javascript/hangman.js
@@ -0,0 +1,99 @@
+var can_play = true;
+var words = new Array("PIVAS", "KOMENDA", "SHARA", "VERAPETROVNA", "SESSIA", "DOPKA");
+
+var to_guess = "";
+var display_word = "";
+var used_letters = "";
+var wrong_guesses = 0;
+var max_wrong_quesses = 10;
+
+function selectLetter(l)
+{
+	//alert(player.energy);
+	if (can_play == false) {
+		return;
+	}
+
+	if (used_letters.indexOf(l) != -1) {
+		return;
+	}
+	
+	used_letters += l;
+	document.game.usedLetters.value = used_letters;
+	
+	if (to_guess.indexOf(l) != -1) {
+		 // correct letter guess
+		pos = 0;
+		temp_mask = display_word;
+
+
+		while (to_guess.indexOf(l, pos) != -1) {
+			pos = to_guess.indexOf(l, pos);			
+			end = pos + 1;
+
+			start_text = temp_mask.substring(0, pos);
+			end_text = temp_mask.substring(end, temp_mask.length);
+
+			temp_mask = start_text + l + end_text;
+			pos = end;
+		}
+
+		display_word = temp_mask;
+		document.game.displayWord.value = display_word;
+			
+		if (display_word.indexOf("#") == -1) {
+			let modalBody = document.getElementsByClassName("modal-body");
+            modalBody[0].getElementsByTagName("p")[0].innerHTML = "Конгратюлейшн. Ти виграв шавуху :";
+            modalBody[0].getElementsByTagName("img")[0].style.display = "block";
+			$('#exampleModalCenter').modal('show');
+
+			can_play = false;
+		}
+	}
+	else {
+		// incortect letter guess
+		wrong_guesses += 1;
+		eval("document.hm.src=\"hangman_pictures/hm" + wrong_guesses + ".gif\"");
+		
+		if (wrong_guesses == max_wrong_quesses) {
+		// lost
+            let modalBody = document.getElementsByClassName("modal-body");
+            modalBody[0].getElementsByTagName("p")[0].innerHTML = "Loshara.";
+            $('#exampleModalCenter').modal('show');
+			can_play = false;
+		}
+	}
+}
+
+function redirect(page) {
+    location.replace(page)
+}
+function reset() {
+	selectWord();
+	document.game.usedLetters.value = "";
+	used_letters = "";
+	wrong_guesses = 0;
+	document.hm.src="hangman_pictures/hmstart.gif";
+}
+
+function selectWord() {
+	can_play = true;
+	random_number = Math.round(Math.random() * (words.length - 1));
+	to_guess = words[random_number];
+	//document.game.theWord.value = to_guess;
+
+	masked_word = createMask(to_guess);
+	document.game.displayWord.value = masked_word;
+	display_word = masked_word;
+}
+
+function createMask(m) {
+	mask = "";
+	word_lenght = m.length;
+
+
+	for (i = 0; i < word_lenght; i ++) {
+		mask += "#";
+	}
+	return mask;
+}
